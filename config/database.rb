b96bb28e095ded1652bc0eb64559c844b6196bb1
catatsuy <catatsuy@catatsuy.org>
2014-03-16 05:09:36 +0900

    db settings

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,7 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("mysql2://localhost/padrino_my_templ_development", :loggers => [logger])
+  when :development then Sequel.connect("mysql2://testuser:passpass@localhost:3306/testdb", :loggers => [logger])
   when :production  then Sequel.connect("mysql2://localhost/padrino_my_templ_production",  :loggers => [logger])
   when :test        then Sequel.connect("mysql2://localhost/padrino_my_templ_test",        :loggers => [logger])
 end
